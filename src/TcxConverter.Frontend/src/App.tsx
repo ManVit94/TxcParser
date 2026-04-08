43b59f809f29c41aa2bf6c3db17b360b5f5f0cc5
Vitaliy Mandzyak <manvit94@gmail.com>
2026-04-08 21:42:55 +0300

    added icon

--- src/TcxConverter.Frontend/src/App.tsx
+++ src/TcxConverter.Frontend/src/App.tsx
@@ -122,6 +122,7 @@ function App() {
       <div className="background-glow"></div>
       
       <header className="header">
+        <img src="/icon.png" alt="App Logo" className="app-logo" />
         <h1>TCX to JSON</h1>
         <p>Convert your Garmin TCX activities into beautiful JSON payloads</p>
       </header>
